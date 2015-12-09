cebe0e34c868cb946e5d5977f392779f4545d5d6
Giselle Pacheco <gpach002@fiu.edu>
2015-12-09 17:54:33 -0500

    Fixed MainMenu Controller

--- RoomReservation/MainMenuViewController.m
+++ RoomReservation/MainMenuViewController.m
@@ -12,9 +12,34 @@
 
 @implementation MainMenuViewController
 
+@synthesize cancelButton, resInfo;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    context = [appDelegate managedObjectContext];
+    
+    NSManagedObject *room = [NSEntityDescription insertNewObjectForEntityForName: @"Reservation" inManagedObjectContext: context];
+    
+    NSLog(@"hello");
+    
+    
+    [room setValue:@"1234" forKey:@"rID"];
+    [room setValue:@"536A" forKey:@"roomNum"];
+    
+    [room setValue:@"YES" forKey:@"taken"];
+    [room setValue:@"2:00-$:59" forKey:@"time"];
+    
+    NSError *err = nil;
+    
+    if([context save:&err]){
+        NSLog(@"Yay?");
+    }
+    else{
+        NSLog(@"FAILURE");
+    }
     
 }
 
@@ -31,6 +56,60 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+ 
+ */
+ 
+ 
 
+
+- (IBAction)pressCancel:(id)sender {
+    
+    UIAlertView *alert;
+    
+    if ([resInfo.text length] == 0) {
+        
+        alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"You do not have any reservations" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        
+        [alert show];
+        
+    } else {
+        alert = [[UIAlertView alloc] initWithTitle:@"Cancel Reservation" message:@"Are you sure you want to cancel your reservation?" delegate:self cancelButtonTitle:@"No" otherButtonTitles:@"Yes", nil];
+        
+        [alert show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    context = [appDelegate managedObjectContext];
+    
+    NSFetchRequest *rq = [[NSFetchRequest alloc] init];
+    NSEntityDescription *desc = [NSEntityDescription entityForName:@"Reservation" inManagedObjectContext:context];
+    [rq setEntity:desc];
+    [rq setResultType:NSDictionaryResultType];
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"taken == %@", @"YES"];
+    [rq setPredicate:pred];
+    
+    NSError *err;
+    
+    NSArray *objects = [context executeFetchRequest:rq error:&err];
+    NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+    
+    if([title isEqualToString:@"No"])
+    {
+        
+    }
+    else
+    {
+        if([title isEqualToString:@"Yes"] && [objects count] > 0)
+        {
+            resInfo.text = @"";
+            [[objects objectAtIndex:0] setValue:@"0" forKey:@"rID"];
+            [[objects objectAtIndex:0] setValue:@"NO" forKey:@"taken"];
+        }
+        
+    }
+}
 @end
+
